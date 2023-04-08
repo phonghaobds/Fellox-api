25a746e9616847abfe58d0da02103a0928a79193
phong47047 <phong47047@donga.edu.vn>
2023-04-08 14:52:31 +0700

    crud apis

---
+++ src/controllers/column.controller.js
@@ -0,0 +1,25 @@
+import { HttpStatusCode } from "*/utilities/constants";
+import { ColumnSerVice } from "*/services/column.service";
+
+const createNew = async (req, res) => {
+  try {
+    const result = await ColumnSerVice.createNew(req.body);
+    res.status(HttpStatusCode.OK).json(result);
+  } catch (error) {
+    res.status(HttpStatusCode.INTERNAL_SERVER).json({
+      errors: error.message,
+    });
+  }
+};
+const update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await ColumnSerVice.update(id, req.body);
+    res.status(HttpStatusCode.OK).json(result);
+  } catch (error) {
+    res.status(HttpStatusCode.INTERNAL_SERVER).json({
+      errors: error.message,
+    });
+  }
+};
+export const ColumnController = { createNew, update };
