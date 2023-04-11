42f2ca05598495e446c0e7e1b5c28470dd776e5a
phong47047 <phong47047@donga.edu.vn>
2023-04-11 12:52:21 +0700

    Combine api get full board, handle cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+import { corsOptions } from "*/config/cors";
 import { connectDB } from "*/config/mongodb";
 import { env } from "*/config/environtment";
 import { apiV1 } from "*/routes/v1";
@@ -14,6 +16,10 @@ connectDB()
 
 const bootServer = () => {
   const app = express();
+
+  
+
+  app.use(cors(corsOptions));
 
   // Enable req.body data
   app.use(express.json());
